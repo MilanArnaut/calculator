06643105aa7a4dfbc5a5a5d5fa1c6226b143f4ea
MilanArnaut <milanarnaut02@gmail.com>
2022-09-06 15:20:33 +0200

    Added functionality for number and erasing buttons

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const deleteButton = document.querySelector('#deleteButton');
+const clearButton = document.querySelector('#clearButton'); 
+const decimalButton = document.querySelector('#decimalButton');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const display = document.querySelector('.display');
+
 const add = (a,b) => {
     return a+b;
 }
@@ -22,3 +29,32 @@ const operate = (num1, num2, operator) => {
         case '/': return divide(num1, num2);
     }
 }
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', e => {
+        e.preventDefault();
+
+        if(display.innerText === '0') display.innerText = '';
+        display.innerText += button.innerText;
+    });
+});
+
+deleteButton.addEventListener('click', e => {
+    e.preventDefault();
+
+    let text = display.innerText;
+    if(text.length === 1) {
+        display.innerText = '0';
+    }
+    else {
+        text = text.substring(0, text.length - 1);
+        display.innerText = text;
+    }
+});
+
+clearButton.addEventListener('click', e => {
+    e.preventDefault();
+
+    display.innerText = '0';
+});
+
